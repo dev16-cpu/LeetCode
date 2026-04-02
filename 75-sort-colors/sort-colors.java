04bfbab5b67726b146fd52824cb4b15193981e62
Dev Gangwar <dev.gangwar_cs.aiml24@gla.ac.in>
2026-04-02 09:00:43 +0530

    Time: 2 ms (18.94%) | Memory: 42.9 MB (99.47%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                int temp=nums[i];
+                if(nums[i]>nums[j])
+                {
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+
+                }
+            }
+        }
+        
+    }
+}
