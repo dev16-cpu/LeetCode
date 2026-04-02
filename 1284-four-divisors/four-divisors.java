85410b92fb66b94cb5308126edb0a2939ab89275
Dev Gangwar <dev.gangwar_cs.aiml24@gla.ac.in>
2026-04-02 08:34:27 +0530

    Time: 1899 ms (7.74%) | Memory: 46.1 MB (87.40%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+         int newsum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            int sum=0;
+            int count=0;
+           
+            for(int j=1;j<=nums[i];j++)
+            {
+                if(nums[i]%j==0){
+                    sum+=j;
+                    count++;
+                }
+                if(count>4) break;
+            }
+            if(count==4) newsum+=sum;
+        }
+        return newsum;
+        
+    }
+}
