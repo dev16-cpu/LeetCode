bbbe0826c8b9e65d2e316f65b3ba50ff55f1c37d
Dev Gangwar <dev.gangwar_cs.aiml24@gla.ac.in>
2026-04-24 10:33:32 +0530

    Time: 1 ms (99.91%) | Memory: 44.5 MB (29.74%) - LeetSync

---
+++ 4229-trim-trailing-vowels/trim-trailing-vowels.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String trimTrailingVowels(String s) {
+        StringBuilder sb= new StringBuilder(s);
+        for(int i=sb.length()-1;i>=0;i--)
+        {
+            char ch= sb.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i'||ch=='o'||ch=='u')
+            {
+                sb.deleteCharAt(i);
+            }
+            else break;
+        }
+
+        return sb.toString();
+        
+            
+        
+        
+    }
+}
