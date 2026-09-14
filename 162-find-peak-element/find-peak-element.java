679726c17835d128862741d97f1357119c0627ac
Dev Gangwar <dev.gangwar_cs.aiml24@gla.ac.in>
2026-09-14 10:23:24 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (20.04%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,16 +1,17 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int max=Integer.MIN_VALUE;
-        int p=0;
-        for(int i=0;i<nums.length;i++)
+        int left=0;
+        int right=nums.length-1;
+        while(left<right)
         {
-            if(nums[i]>max)
+            int mid=left+(right-left)/2;
+            if(nums[mid]>nums[mid+1])
             {
-                max=nums[i];
-                p=i;
+                right=mid;
             }
+            else left=mid+1;
         }
-        return p;
         
+        return left;
     }
 }
