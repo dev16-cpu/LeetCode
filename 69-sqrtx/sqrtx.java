509c95a61fd806b736dc8c85dad6eb691fead8d7
Dev Gangwar <dev.gangwar_cs.aiml24@gla.ac.in>
2026-09-21 10:46:21 +0530

    Time: 57 ms (5.24%) | Memory: 42.5 MB (78.76%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -2,7 +2,7 @@ import java.math.*;
 class Solution {
     public int mySqrt(int x) {
         if(x==0) return 0;
-        for(int i=0;i<x;i++)
+        for(int i=0;i<=x/2+1;i++)
         {
          
             if((long)i*i>x) return i-1;
